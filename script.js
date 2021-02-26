152b57073e7df1e8e26657f8aea36f0546ec056f
warleon <51892390+warleon@users.noreply.github.com>
2021-02-26 16:28:13 -0500

    done

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-var block= document.getElementById("block");
-var character= document.getElementById("character");
+const block= document.getElementById("block");
+const hole= document.getElementById("hole");
+const player= document.getElementById("player");
+var jumping = 0;
+
+hole.addEventListener('animationiteration', ()=>{
+    var random = -((Math.random()*300)+150);
+    hole.style.top = random + "px";
+});
+
+setInterval(()=>{
+    var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    var cTop= playerTop-500;
+    if(playerTop<457 && jumping==0){
+        player.style.top = (playerTop+3)+"px";
+    }
+    if(playerTop>455 || ( (blockLeft<50) && (blockLeft>-50) && ( (cTop<holeTop) || (cTop>holeTop+110) ) ) ){
+        alert("game over");
+        player.style.top = "50px";
+    }
+},10);
 
 function jump(){
-    if(character.classList != "anim")
-        character.classList.add("anim");
-    setTimeout(function(){
-    character.classList.remove("anim");
-    },500);
-}
-
-var checkDead = function(){
-    var ctop= parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    var bleft= parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-
-    if(bleft>0 && bleft<20 && ctop>130){
-        block.style.animation= "none";
-        if(confirm("you lose, wanna play again?")){
-            block.style.animation= "block 2s infinite linear";
+    jumping =1;
+    var jumpCount = 0;
+    var jumpIntervale = setInterval(()=>{
+        var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+        if(playerTop>6 && jumpCount<15){
+            player.style.top = (playerTop-5)+"px";
         }
-    }
+        if(jumpCount>20){
+            clearInterval(jumpIntervale);
+            jumping=0;
+            jumpCount=0;
+        }
+        jumpCount++;
+    },10);
 }
-
-var repite = setInterval(checkDead,10);
